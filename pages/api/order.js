e33e81e77a1833ca087c9802f04d9f7c2443991b
Gustavo <58173403+GFrainer@users.noreply.github.com>
2021-05-13 07:34:06 -0300

    Update order.js

--- pages/api/order.js
+++ pages/api/order.js
@@ -19,7 +19,7 @@ const symbols = {
 
 export default (req, res) => {
     var symbol = req.body
-    console.log(JSON.parse(symbol))
+    console.log(req.body)
     res.status(200).json({res: "New order: "});
     return
     symbol = {
