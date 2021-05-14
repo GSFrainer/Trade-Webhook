a0206ac237baa0e3d472f45c89a4137a7d018504
Gustavo <58173403+GFrainer@users.noreply.github.com>
2021-05-14 00:36:47 -0300

    2Way

--- pages/api/order.js
+++ pages/api/order.js
@@ -19,7 +19,7 @@ const symbols = {
 
 export default (req, res) => {
     var symbol = req.body
-    // symbol = JSON.parse('{  "Price": 0.11821,  "Stop": 0.11794,  "High": 0.11821,  "Low": 0.11794,  "Side": "BUY",  "Symbol": "BTC/USDT"}')
+    //var symbol = JSON.parse('{  "Price": 0.11821,  "Stop": 0.11794,  "High": 0.11821,  "Low": 0.11794,  "Side": "BUY",  "Symbol": "BTC/USDT"}')
     // console.log(req.body)
     symbol.Side = symbol.Side.toLowerCase()
     // console.log(symbol['Price'])
@@ -37,26 +37,36 @@ export default (req, res) => {
     // binance.fetchBalance().then(a=>res.status(200).json({res: a['info']['positions'].find(a=>(a['symbol'] == symbol.Symbol.replace('/','') && a['entryPrice'] != 0.0))}))
     
     
-    // binance.fetchBalance().then(a=>{
-    //     var position = a['info']['positions'].find(a=>(a['symbol'] == symbol.Symbol.replace('/','') && a['entryPrice'] != 0.0))
-    //     console.log(position)
-    // })
+    binance.fetchBalance().then(a=>{
+        var position = a['info']['positions'].find(a=>(a['symbol'] == symbol.Symbol.replace('/','') && a['entryPrice'] != 0.0))
+        console.log(position)
+        
+        var quantity = symbols[symbol.Symbol]
+        if(position != undefined){
+            quantity = symbols[symbol.Symbol]*2
+        }
+        
+        binance.createOrder(symbol.Symbol, 'MARKET', symbol.Side, symbols[symbol.Symbol], undefined, {}).then(o=>{
+            res.status(200).json({res: "New order: " + symbol.Symbol});
+        //     binance.createOrder(symbol.Symbol, 'TRAILING_STOP_MARKET', (symbol.Side == "buy" ? "sell" : "buy"), symbols[symbol.Symbol], undefined, {'callbackRate': 1.0, "reduceOnly": true}).then(t => {
+        //         binance.createOrder(symbol.Symbol, 'STOP', (symbol.Side == "buy" ? "sell" : "buy"), symbols[symbol.Symbol], symbol.Stop * 2, {'stopPrice': symbol.Stop, "reduceOnly": true}).then(s => {
+        //             res.status(200).json({res: "New order: " + symbol.Symbol});
+        //         }).catch(e => {
+        //             res.status(400).json({res: "e"})
+        //         })
+        //     }).catch(e => {
+        //         binance.createOrder(symbol.Symbol, 'MARKET', (symbol.Side == "buy" ? "sell" : "buy"), symbols[symbol.Symbol], undefined, {"reduceOnly": true})
+        //         res.status(400).json({res: "e"})
+        //     })
+        }).catch(e=>{
+            console.log(e)
+            res.status(400).json({res: "e"})
+        })
+    })
     
     //binance.fetchOpenOrders ("BTC/USDT").then(a=>console.log(a))
     // return res.status(200).json({res: "New order"})
-
-    binance.createOrder(symbol.Symbol, 'MARKET', symbol.Side, symbols[symbol.Symbol], undefined, {}).then(o=>{
-        binance.createOrder(symbol.Symbol, 'TRAILING_STOP_MARKET', (symbol.Side == "buy" ? "sell" : "buy"), symbols[symbol.Symbol], undefined, {'callbackRate': 1.0, "reduceOnly": true}).then(t => {
-            binance.createOrder(symbol.Symbol, 'STOP', (symbol.Side == "buy" ? "sell" : "buy"), symbols[symbol.Symbol], symbol.Stop * 2, {'stopPrice': symbol.Stop, "reduceOnly": true}).then(s => {
-                res.status(200).json({res: "New order: " + symbol.Symbol});
-            }).catch(e => {
-                res.status(400).json({res: "e"})
-            })
-        }).catch(e => {
-            binance.createOrder(symbol.Symbol, 'MARKET', (symbol.Side == "buy" ? "sell" : "buy"), symbols[symbol.Symbol], undefined, {"reduceOnly": true})
-            res.status(400).json({res: "e"})
-        })
-    }).catch(e=>{res.status(400).json({res: "e"})})
+    
 }
 
 async function test() {
