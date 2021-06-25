47c2ba98cd593e6be66e74d515e4f9267bc6237e
Gustavo <58173403+GFrainer@users.noreply.github.com>
2021-06-25 02:14:21 -0300

    Update order.js

--- pages/api/order.js
+++ pages/api/order.js
@@ -16,72 +16,46 @@ const symbols = {
     'BTC/USDT': 0.001,
     'BNB/USDT': 0.01
 }
-//6.03
+
+//Request Body: {"Price":{{close}}, "Stop":{{low}}, "High":{{high}}, "Low":{{low}}, "Side":"BUY", "Symbol":"ADA/USDT"}
 export default (req, res) => {
     var symbol = req.body
-    //var symbol = JSON.parse('{  "Price": 0.11821,  "Stop": 0.11794,  "High": 0.11821,  "Low": 0.11794,  "Side": "BUY",  "Symbol": "BTC/USDT"}')
-    // console.log(req.body)
     symbol.Side = symbol.Side.toLowerCase()
-    // console.log(symbol['Price'])
-    // console.log(symbol['Stop'])
     symbol.Stop = (symbol.Side=='buy'?(parseFloat(symbol.Price)*0.985):(parseFloat(symbol.Price)*1.015))
     if(Number.isInteger(symbol['High']) && Number.isInteger(symbol['Low'])){
-        //symbol.Stop = parseInt(symbol.Stop) + (symbol.Side=='buy'?(parseInt(symbol.Low) - parseInt(symbol.High)):(parseInt(symbol.High) - parseInt(symbol.Low)))
         symbol.Stop = Math.trunc(symbol.Stop)
     }else{
         symbol.Stop = symbol.Stop.toFixed((""+symbol.Price).split('.')[1].length)
     }
-    // console.log(symbol.Stop)
-    // console.log(symbol['Side'])
     
-    
-    // binance.fetchBalance().then(a=>res.status(200).json({res: a['info']['positions'].find(a=>(a['symbol'] == symbol.Symbol.replace('/','') && a['entryPrice'] != 0.0))}))
-    console.log(symbol)
-    //return
     binance.fetchBalance().then(a=>{
-        var position = a['info']['positions'].find(a=>(a['symbol'] == symbol.Symbol.replace('/','') && a['entryPrice'] != 0.0))
-        console.log(position)
-        
-        var quantity = symbols[symbol.Symbol]
-        if(position != undefined){
-            quantity = symbols[symbol.Symbol]*2
+        if(symbol.Quantity == undefined){
+            symbol.Quantity = symbols[symbol.Symbol]
         }
-        
-        binance.createOrder(symbol.Symbol, 'MARKET', symbol.Side, quantity, undefined, {}).then(o=>{
+
+        // -- Revert position side --
+        // var position = a['info']['positions'].find(a=>(a['symbol'] == symbol.Symbol.replace('/','') && a['entryPrice'] != 0.0))
+        // if(position != undefined){
+        //     symbol.Quantity = symbol.Quantity*2
+        // }
+
+        binance.createOrder(symbol.Symbol, 'MARKET', symbol.Side, symbol.Quantity, undefined, {}).then(o=>{
             res.status(200).json({res: "New order: " + symbol.Symbol});
-            binance.createOrder(symbol.Symbol, 'TRAILING_STOP_MARKET', (symbol.Side == "buy" ? "sell" : "buy"), quantity, undefined, {'callbackRate': 1.0, "reduceOnly": true}).then(t => {
-        //         binance.createOrder(symbol.Symbol, 'STOP', (symbol.Side == "buy" ? "sell" : "buy"), quantity, symbol.Stop * 2, {'stopPrice': symbol.Stop, "reduceOnly": true}).then(s => {
-        //             res.status(200).json({res: "New order: " + symbol.Symbol});
-        //         }).catch(e => {
-        //             res.status(400).json({res: "e"})
-        //         })
-             }).catch(e => {
-                 //binance.createOrder(symbol.Symbol, 'MARKET', (symbol.Side == "buy" ? "sell" : "buy"), symbols[symbol.Symbol], undefined, {"reduceOnly": true})
-                 res.status(400).json({res: "e"})
-             })
+
+            // -- Stop Loss and Trailing Stop Orders --
+            // binance.createOrder(symbol.Symbol, 'STOP', (symbol.Side == "buy" ? "sell" : "buy"), symbol.Quantity, symbol.Stop * 2, {'stopPrice': symbol.Stop, "reduceOnly": true}).then(s => {
+            //     res.status(200).json({res: "New order: " + symbol.Symbol});
+            // }).catch(e => {
+            //     res.status(400).json({res: "e"})
+            // })
+            // }).catch(e => {
+            //     //binance.createOrder(symbol.Symbol, 'MARKET', (symbol.Side == "buy" ? "sell" : "buy"), symbol.Quantity, undefined, {"reduceOnly": true})
+            //     res.status(400).json({res: e})
+            // })
+
         }).catch(e=>{
             console.log(e)
-            res.status(400).json({res: "e"})
+            res.status(400).json({res: e})
         })
-    })
-    
-    //binance.fetchOpenOrders ("BTC/USDT").then(a=>console.log(a))
-    // return res.status(200).json({res: "New order"})
-    
-}
-
-async function test() {
-    const localStartTime = Date.now()
-    const {serverTime} = await binance.publicGetTime()
-    const localFinishTime = Date.now()
-    const estimatedLandingTime = (localFinishTime + localStartTime) / 2
-
-    const diff = serverTime - estimatedLandingTime
-    return diff;
-}
-
-async function fetchOrders() {
-    binance.fetchOrders('BTC/USDT', undefined, undefined, {"recvWindow": 10000}).then(l => {
-        res.status(200).json({res: l});
-    })
+    })   
 }
